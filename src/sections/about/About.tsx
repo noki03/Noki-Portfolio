a8faebd512e399f062b2111b2b999e69dd69e2c8
rhys.salvador <rhys.salvador@noemail.cc>
2025-10-30 11:13:35 +0800

    fix: added Section Fades, section classname adjusted the vertical flex

--- src/sections/about/About.tsx
+++ src/sections/about/About.tsx
@@ -4,22 +4,33 @@ import AboutBadge from "./AboutBadge";
 import AboutText from "./AboutText";
 import AboutSkills from "./AboutSkills";
 import AboutVisual from "./AboutVisual";
-
+import FadeInSection from "../../components/motion/FadeInSection";
 const About: React.FC = () => {
   return (
     <section
       id="about"
-      className="relative min-h-screen flex items-center justify-center text-neutral-100 py-28 px-6 md:px-12 overflow-hidden"
+      className="relative min-h-screen flex flex-col justify-center md:justify-start pt-24 text-neutral-100 px-6 md:px-12 overflow-hidden"
     >
       <AboutBackgrounds />
 
       <div className="relative max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-center z-10">
         <div className="space-y-6">
-          <AboutBadge />
-          <AboutText />
-          <AboutSkills />
+          <FadeInSection delay={0.1}>
+            <AboutBadge />
+          </FadeInSection>
+
+          <FadeInSection retrigger delay={0.2}>
+            <AboutText />
+          </FadeInSection>
+
+          <FadeInSection retrigger delay={0.3}>
+            <AboutSkills />
+          </FadeInSection>
         </div>
-        <AboutVisual />
+
+        <FadeInSection retrigger delay={0.4}>
+          <AboutVisual />
+        </FadeInSection>
       </div>
     </section>
   );
