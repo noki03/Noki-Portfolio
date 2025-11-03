876618341f0a622a8c284becdb7b4fbbebb08187
rhys.salvador <rhys.salvador@noemail.cc>
2025-11-04 07:35:45 +0800

    feat: removed temp description and badge details

--- src/sections/hero/HeroDescription.tsx
+++ src/sections/hero/HeroDescription.tsx
@@ -2,14 +2,11 @@ import React from "react";
 
 const HeroDescription: React.FC = () => (
   <p className="text-xl md:text-2xl text-neutral-200 mb-10 max-w-3xl mx-auto leading-relaxed font-light">
-    Lorem{" "}
-    <span className="text-primary-300 font-medium">ipsum dolor sit amet,</span>{" "}
-    <span className="text-secondary-300 font-medium">
-      consectetur adipiscing
-    </span>{" "}
-    elit. Integer facilisis lacus ut nisi aliquam,{" "}
-    <span className="text-accent-300 font-medium">nec varius tortor</span>{" "}
-    viverra.
+    I <span className="text-primary-300 font-medium">create</span> and{" "}
+    <span className="text-secondary-300 font-medium">innovate</span> alongside{" "}
+    <span className="text-accent-300 font-medium">technology</span> — coding
+    smarter, designing intuitively, and adapting fast in the era of intelligent
+    tools.
   </p>
 );
 
